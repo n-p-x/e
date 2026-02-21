8d20ad204f71f95f9ca897a53a2047da636c3e4e
Karachvi <115191539+karachvi@users.noreply.github.com>
2026-02-21 06:40:47 +0500

    Replace lodash with Handlebars and helpers

--- test.js
+++ test.js
@@ -1,9 +1,26 @@
-const deps = ['lodash'];
+const deps = ['handlebars', 'handlebars-helpers', 'handlebars-helper-repeat'];
 
-import _ from 'lodash';
+import Handlebars from "handlebars";
+import repeat from "handlebars-helper-repeat";
+import helpers from "handlebars-helpers";
+import fs from "fs";
+import path from "path";
+
+Handlebars.registerHelper('repeat', repeat);
+
+helpers({
+  handlebars: Handlebars
+});
 
 function script({ positional, named }) {
-  console.log('Lodash version:', _.VERSION);
-  console.log('Positional:', positional);
-  console.log('Named:', named);
+  if(positional.length < 3) throw new Error("Two positional args required");
+  const jsonf = path.resolve(positional[0]);
+  const tplf = path.resolve(positional[1]);
+  const outf = path.resolve(positional[2]);
+  const json = fs.readFileSync(jsonf, 'utf8');
+  const tpl = fs.readFileSync(tplf, 'utf8');
+  const template = Handlebars.compile(tpl);
+  const output = template(JSON.parse(json));
+  fs.writeFileSync(outf, output, 'utf8');
+  console.log('Output:', output);
 }
